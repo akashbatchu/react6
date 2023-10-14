b0c54899239f9d37db66e9c242899754ad89307a
akashbatchu <akashbatchu2003@gmail.com>
2023-10-14 23:59:40 +0530

    first commit

---
+++ src/components/Clubs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Clubs = () => {
+  return (
+    <section>
+            <h2>Clubs & Organizations</h2>
+            <ul>
+                <li>Digit squad (Technical) - Core Member - 2021</li>
+                <li> Worked with the Hall Arrangements team for RIVIERA 2023.</li>
+            </ul>
+    </section>
+  );
+}
+
+export default Clubs;
